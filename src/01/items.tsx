3e21fb89836ac8a9cf5bcc3edfd38ea4bb30c473
BorisPanich <panich2303@gmail.com>
2021-11-04 06:23:45 +0300

    created items

---
+++ src/01/items.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import { api } from "./api"
+
+const ItemsPage = () => {
+    const [items, setItems] = useState([])
+    const [status, setStatus] = useState('idle')
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        setStatus('loading')
+        setError(null)
+        api.loadItems().then((res) => {
+
+        }).catch(err => {
+            setError(err)
+        }).finally(() => setStatus('idle'))
+
+    }, [])
+
+    const deleteItem = (id: any) => {
+        setStatus('loading')
+        api.deleteItem(id).then((res) => {
+            setItems(items.filter(i => i.id !== id))
+        }).catch(err => {
+            setError(err)
+        }).finally(() => setStatus('idle'))
+    }
+
+    return <div>
+        {(status === 'loading') && <span>Loading...</span>}
+        {!!error && <span>{error.toString()}</span>}
+        <ul> {
+            items.map(i => {
+                return <Item item={i}
+                    deleteItem={deleteItem}
+                    key={i.id} />
+            })}
+        </ul>
+    </div>
+
+}
+
+const Item = (props: any) => <li>
+    {props.item.title}
+    <button onClick={() => { props.deleteItem(props.item.id) }} >X</button>
+</li>
